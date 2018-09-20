dbbada8b167596312115e5c4e99c23e23615e47d
marcialwushu <marcialwushu@gmail.com>
2018-09-20 16:07:37 -0300

    ReferenceField component added with insert User link

--- src/posts.js
+++ src/posts.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { List, Datagrid, TextField } from 'react-admin';
+import { List, Datagrid, TextField, EmailField, ReferenceField } from 'react-admin';
 
 export const PostList = (props) => (
     <List {...props}>
         <Datagrid>
-            <TextField source="id"></TextField>
-            <TextField source="title"></TextField>
-            <TextField source="body"></TextField>
+            <TextField source="id" />
+            <ReferenceField label="User" source="userId" reference="users">
+                <TextField source="name"/>
+            </ReferenceField>
+            <TextField source="title" />
+            <TextField source="body" />
         </Datagrid>
     </List>
 );
