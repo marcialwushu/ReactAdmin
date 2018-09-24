3f8970d6fff88254c19e60a6b84436b5f17b8083
marcialwushu <marcialwushu@gmail.com>
2018-09-24 17:33:04 -0300

    connecting to real api - in progress

---
+++ src/dataProvider.js
@@ -0,0 +1,15 @@
+import {
+    GET_LIST,
+    GET_ONE,
+    GET_MANY,
+    GET_MANY_REFERENCE,
+    CREATE,
+    UPDATE,
+    UPDATE,
+    DELETE,
+    fetchUtils,
+} from 'react-admin';
+import { stringify } from 'query-string';
+
+const API_URL = 'my.api.url';
+
