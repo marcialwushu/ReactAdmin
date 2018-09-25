c1729e6c7fcb00a129956bdf8c011cc20c398a16
marcialwushu <marcialwushu@gmail.com>
2018-09-25 10:47:44 -0300

    const DataProviderRequestToHTTP added with GET_LIST, GET_ONE and GET_MANY methods

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -13,3 +13,25 @@ import { stringify } from 'query-string';
 
 const API_URL = 'my.api.url';
 
+const convertDataProviderRequestToHTTP = (type, resource, params) => {
+    switch (type) {
+    case GET_LIST: {
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const query = {
+            sort: JSON.stringify([field, order]),
+            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+            filter: JSON.stringify(params.filter),
+        };
+        return { url: `${API_URL}/${resource}?${stringify(query)}` };
+    }
+    case GET_ONE:
+        return { url: `${API_URL}/${resource}/${params.id}` };
+    case GET_MANY: {
+        const query = {
+            filter: JSON.stringify({ id: params.ids }),
+        };
+        return { url: `${API_URL}/${resource}?${stringify(query)}` };
+    }
+    }
+}
